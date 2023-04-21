19ccf3aa63ad7e25bd18aef0a509aedac2b4aa00
Pranjal Walia <pranjalwalia77@gmail.com>
2023-04-22 02:29:21 +0530

    feat: add revision history + rollout conf

--- scripts/rollout-infra.sh
+++ scripts/rollout-infra.sh
@@ -1,3 +1,3 @@
 kubectl apply -f infra/k8s -n referit
-# kubectl rollout restart deployment.apps/referit-frontend-deployment -n referit
-# kubectl rollout status deployment.apps/referit-frontend-deployment -n referit
+kubectl rollout restart deployment.apps/referit-frontend-deployment -n referit
+kubectl rollout status deployment.apps/referit-frontend-deployment -n referit
