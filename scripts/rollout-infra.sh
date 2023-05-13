e2eb46ece718e62c9b0882ce1a66fe0050e5bb5a
Samaksh Dhingra <55679277+samakshd@users.noreply.github.com>
2023-05-13 18:00:01 +0530

    Update rollout-infra.sh

--- scripts/rollout-infra.sh
+++ scripts/rollout-infra.sh
@@ -6,6 +6,6 @@ kubectl apply -f infra/k8s/referit-frontend-configmap.yaml -n referit
 echo "Waiting for Configuration..."
 sleep 5
 
-kubectl apply -f infra/k8s/referit-frontend-deployment.yaml -n referit
+kubectl apply -f infra/k8s/referit-frontend.yaml -n referit
 kubectl rollout restart deployment.apps/referit-frontend-deployment -n referit
 kubectl rollout status deployment.apps/referit-frontend-deployment -n referit
