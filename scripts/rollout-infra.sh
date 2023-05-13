28b86cf3518e10fa58257f737b1f5c5aee3a9379
Pranjal Walia <pranjalwalia77@gmail.com>
2023-05-13 16:14:52 +0530

    feat: add updates manifests

--- scripts/rollout-infra.sh
+++ scripts/rollout-infra.sh
@@ -1,3 +1,11 @@
-kubectl apply -f infra/k8s -n referit
+kubectl apply -f infra/k8s/referit-frontend-secret.yaml -n referit
+echo "Waiting for Configuration..."
+sleep 5 
+
+kubectl apply -f infra/k8s/referit-frontend-configmap.yaml -n referit
+echo "Waiting for Configuration..."
+sleep 5
+
+kubectl apply -f infra/k8s/referit-frontend-deployment.yaml -n referit
 kubectl rollout restart deployment.apps/referit-frontend-deployment -n referit
 kubectl rollout status deployment.apps/referit-frontend-deployment -n referit
